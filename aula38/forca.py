639661574addf47d11641cbce00e5085709ca009
AlessanderJ <alessander.santos@outlook.com.br>
2022-08-30 10:37:34 -0300

    commit-30-08-2022

--- aula38/forca.py
+++ aula38/forca.py
@@ -3,32 +3,44 @@ import enum
 
 palavra = 'samambaia'
 newPalavra = ''
+
+print('-='*40)
+print(' ')
+print(f'{"JOGO DA FORCA":^40}')
+print(' ')
+print('-='*40)
+print(' ')
+print(' ')
+
+LARG_PALAVRA = len(palavra)
+
+print(f'A palavra tem {LARG_PALAVRA} letras.')
+
 while True:
-    print('-='*40)
-    print(' ')
-    print(f'{"JOGO DA FORCA":^40}')
-    print(' ')
-    print('-='*40)
-    print(' ')
-    print(' ')
-
-    LARG_PALAVRA = len(palavra)
-    newPalavra = '_'*LARG_PALAVRA
-
-    print(f'A palavra tem {LARG_PALAVRA} letras.')
     print('Quer escolher uma letra ou falar a palavra?')
     choose = int(input('''
         1- Letra
         2- Palavra
+
         Escolha:  '''))
 
     if choose == 1:
         letra = str(input('Digite uma letra: '))
         for elemento in palavra:
-            if elemento == letra:
-                    elemento+=newPalavra
+            if letra == elemento:
+                    newPalavra += elemento
+                    pass
                 
             else:
-                newPalavra+= '_'
+                newPalavra += '_'
     
+    elif choose == 2:
+        digPalavra = str(input('Digite a palavra: '))
+        if digPalavra == palavra:
+            print('Voce acertou a palavra correta. Parabéns!!!')
+            break
+    
+    print(' ')
+    print('-- STATUS DA PALAVRA --')
     print(newPalavra)
+    print(' ')
